058d0f4b69469a770357f002f54fdfc93bfc24f2
Swathi Kuchukulla <SRKuchukulla@evolenthealth.com>
2019-04-05 15:31:25 -0400

    Modified SetUp file

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='Risk Adjustment Scoring Engine',
     version='0.1', 
     packages=find_packages(),
-    install_requires=[
-    	#python, you should list python depdencies(pip installables, not python itself)
-    	#flask, it's not being used for this project.
-    ],
-    #python_requires='>=3.6', # if you want to target specific python version
     include_package_data=True,
+    install_requires=[
+        python
+    ],
+    python_requires='>=3.6',
+
 )
